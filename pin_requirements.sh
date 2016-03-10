ddf79431761a4efbec96c4a2c5d1883021007dea
Stephan Erb <stephan.erb@blue-yonder.com>
2016-03-10 13:26:05 +0100

    Remove unnecessary index handling in req pinning

--- pin_requirements.sh
+++ pin_requirements.sh
@@ -2,12 +2,6 @@
 
 ## Take the content from requirements.in and compile fully pinned requirements.txt
 
-if [ -z "${1}" ]
-then
-    echo "usage: $0 index_url" > /dev/stderr
-    exit 1
-fi
-
 # Run in virtualenv.
 VENV=$(mktemp -du)
 virtualenv ${VENV}
@@ -21,10 +15,9 @@ trap cleanup EXIT
 pip install 'pip>7.0.0'
 pip install -U 'pip-tools>=1.6'  # 1.6 is the first version to support `--upgrade`.
 
-INDEX="${1}"
 REQ_BASE=requirements
 
 echo "=== Compiling ${REQ_BASE}.txt ==="
 
-pip-compile -i ${INDEX} --upgrade --no-header --no-index ${REQ_BASE}.in -o ${REQ_BASE}.txt
+pip-compile --upgrade --no-header --no-index ${REQ_BASE}.in -o ${REQ_BASE}.txt
 sed -i '1i# DO NOT MODIFY THIS FILE. See `pin_requirements.sh` instead\n' ${REQ_BASE}.txt
