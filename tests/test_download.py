99671ed281439bba3a9b9f759c8f5a9dfe8ad13c
Merkel, Andreas <Andreas.Merkel@blue-yonder.com>
2016-03-14 15:11:00 +0100

    Add test for download from inherited index.

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,7 +2,8 @@ import os
 import unittest
 
 from devpi_plumber.server import TestServer
-from tests.config import NATIVE_PASSWORD, NATIVE_USER
+
+from tests.config import NATIVE_USER, NATIVE_PASSWORD
 from tests.fixture import DIST_DIR, PACKAGE_NAME
 from tests.utils import download
 
@@ -29,20 +30,36 @@ class DownloadTests(unittest.TestCase):
         devpi.upload(DIST_DIR, directory=True)
         self.assertTrue(download(PACKAGE_NAME, devpi.url))
 
+    def assert_inherited_downloads(self, devpi):
+        devpi.login(NATIVE_USER, NATIVE_PASSWORD)
+
+        devpi.create_index('index1')
+        devpi.create_index('index2', bases=NATIVE_USER + '/index1')
+        devpi.create_index('index3', bases=NATIVE_USER + '/index2')
+
+        devpi.use(NATIVE_USER, 'index1')
+        self.assertFalse(download(PACKAGE_NAME, devpi.url))
+        devpi.upload(DIST_DIR, directory=True)
+
+        devpi.use(NATIVE_USER, 'index3')
+        self.assertTrue(download(PACKAGE_NAME, devpi.url))
+
     def test_master(self):
-        users = {NATIVE_USER: {'password': NATIVE_PASSWORD}}
-        indices = {NATIVE_USER + '/index': {'bases': 'root/pypi'}}
+        users = { NATIVE_USER: {'password': NATIVE_PASSWORD} }
+        indices = { NATIVE_USER + '/index' : {'bases': 'root/pypi'} }
 
         with TestServer(users, indices) as devpi:
             self.assert_pypi_downloads(devpi)
             self.assert_internal_downloads(devpi)
+            self.assert_inherited_downloads(devpi)
 
     def test_replica(self):
-        users = {NATIVE_USER: {'password': NATIVE_PASSWORD}}
-        indices = {NATIVE_USER + '/index': {'bases': 'root/pypi'}}
+        users = { NATIVE_USER: {'password': NATIVE_PASSWORD} }
+        indices = { NATIVE_USER + '/index' : {'bases': 'root/pypi'} }
 
-        with TestServer(users, indices, config={'port': 2414}) as master:
+        with TestServer(users, indices, config={'port' : 2414 }) as master:
             with TestServer(config={'master-url': master.url, 'port': 2413}) as replica:
 
                 self.assert_pypi_downloads(replica)
                 self.assert_internal_downloads(replica)
+                self.assert_inherited_downloads(replica)
