75acf6abf9ee861a7d62ad1c9efa37733f808877
Merkel, Andreas <Andreas.Merkel@blue-yonder.com>
2016-03-17 16:28:08 +0100

    Use newer plumber.

--- tests/test_export_import.py
+++ tests/test_export_import.py
@@ -1,7 +1,7 @@
 from twitter.common.contextutil import temporary_dir
 import unittest
 
-from devpi_plumber.server import TestServer
+from devpi_plumber.server import TestServer, export_state, import_state
 
 from tests.config import NATIVE_USER, NATIVE_PASSWORD
 from tests.fixture import DIST_DIR, PACKAGE_NAME
@@ -14,15 +14,17 @@ class ExportImportTests(unittest.TestCase):
         users = {NATIVE_USER: {'password': NATIVE_PASSWORD}}
         indices = {NATIVE_USER + '/index': {'bases': 'root/pypi'}}
 
-        with temporary_dir() as export_dir:
-            with TestServer(users, indices, export=export_dir) as devpi:
-                devpi.login(NATIVE_USER, NATIVE_PASSWORD)
-                devpi.use(NATIVE_USER, 'index')
-                devpi.upload(DIST_DIR, directory=True)
+        with temporary_dir() as state_dir:
+            with temporary_dir() as server_dir1:
+                with TestServer(users, indices, config=dict(serverdir=server_dir1)) as devpi:
+                    devpi.login(NATIVE_USER, NATIVE_PASSWORD)
+                    devpi.use(NATIVE_USER, 'index')
+                    devpi.upload(DIST_DIR, directory=True)
+                export_state(server_dir1, state_dir)
 
-            with TestServer(import_=export_dir) as devpi:
-                devpi.login(NATIVE_USER, NATIVE_PASSWORD)
-                devpi.use(NATIVE_USER, 'index')
-                self.assertTrue(download(PACKAGE_NAME, devpi.url))
-
-
+            with temporary_dir() as server_dir2:
+                import_state(server_dir2, state_dir)
+                with TestServer(config=dict(serverdir=server_dir2)) as devpi:
+                    devpi.login(NATIVE_USER, NATIVE_PASSWORD)
+                    devpi.use(NATIVE_USER, 'index')
+                    self.assertTrue(download(PACKAGE_NAME, devpi.url))
